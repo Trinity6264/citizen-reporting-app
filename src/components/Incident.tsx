283674d217f4629b97ff5765585f23fc3bddc405
Trinity6264 <amoahtnt6@gmail.com>
2025-05-13 11:08:41 +0000

    Add lucide-react dependency and implement incident reporting UI
    - Added "lucide-react" as a dependency in package.json and package-lock.json.
    - Created Incident component for incident types in the reporting form.
    - Updated Home page layout to include incident reporting section and description textarea.
    - Removed dark mode styles from globals.css.

---
+++ src/components/Incident.tsx
@@ -0,0 +1,11 @@
+const Incident = () => {
+    return (
+        <div className="flex flex-col  items-center justify-center border gap-1 border-gray-200 p-2 sm:p-3 md:p-4 rounded-lg text-center hover:bg-gray-50 transition duration-200 cursor-pointer">
+            <span className="text-2xl">💡</span>
+            <span className="text-sm">Broken Streetlight</span>
+        </div>
+    );
+};
+
+export default Incident;
+  
